f345ae3dd5e809dee26019eccb82e617d8d06103
Spencer Ricks <spencerricks@Spencers-MacBook-Pro.local>
2018-08-16 15:22:06 -0600

    =

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,7 +6,7 @@ export default class Login extends Component {
     login(){
         let {REACT_APP_DOMAIN, REACT_APP_CLIENT_ID} = process.env
         let url = encodeURI(`${window.location.origin}/auth/callback`)
-        window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri${url}&response_type=code`
+        window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`
     }
 
     render(){
