8a37a97ff79c28f00ee8afabd00389bff2d42e41
Spencer Ricks <spencerricks@Spencers-MacBook-Pro.local>
2018-08-17 11:36:02 -0600

    landing page changed

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,16 +6,32 @@ import axios from 'axios';
 
 import './LandingPage.css'
 import powerWasher from '../../images/foam-cannon.jpg';
+require('dotenv').config();
 
 export default class LandingPage extends Component {
+    constructor(){
+        super()
 
-    // handlePhotoApi(){
-    //     axios.get(`https://api.unsplash.com/search/photos/?page=1&per_page=10&query=${IMAGE_SEARCH_HERE}&client_id=${REACT_APP_UNSPLASH_PUBLIC_KEY_HERE}`)
-    //         .then(photo => console.log(photo.data))
-    //         .catch(err => console.log(err));
-    // }
+        this.state = {
+            backgroundImage: ''
+        }
+    }
+
+    componentDidMount(){
+        this.handlePhotoApi()
+    }
+
+    async handlePhotoApi(){
+        let IMAGE_SEARCH = 'car'
+        let REACT_APP_UNSPLASH_PUBLIC_KEY = process.env.REACT_APP_UNSPLASH_PUBLIC_KEY
+
+        const photo = await axios.get(`https://api.unsplash.com/search/photos/?page=1&per_page=10&query=${IMAGE_SEARCH}&client_id=${REACT_APP_UNSPLASH_PUBLIC_KEY}`)
+            this.setState({backgroundImage: photo.data.results[0].urls.regular})
+            // .catch(err => console.log(err));
+    }
 
     render (){
+        console.log(this.state)
         return(
             <div>
                 <p>LandingPage (Home)</p>
@@ -28,7 +44,9 @@ export default class LandingPage extends Component {
                     </HashRouter>
                 </div>
                 <div>
-                    <img className='landingPageImage' src={powerWasher} alt="" />
+                    {/* <img className='landingPageImage' src={powerWasher} alt="" /> */}
+                    <img className='landingPageImage' src={this.state.backgroundImage} alt="" />
+                    {/* {JSON.stringify(this.handlePhotoApi)} */}
                 </div>
             </div>
         )
